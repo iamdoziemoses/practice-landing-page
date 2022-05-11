a16d987059a711aebd152c38a3ed7199ee5994a6
Moses Chidozie <80219224+dozeman-moses@users.noreply.github.com>
2022-05-11 19:29:47 +0100

    update files

---
+++ Documents/react-portfolio-master/react-portfolio-master/src/Components/About.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var profilepic= "images/"+this.props.data.image;
+      var bio = this.props.data.bio;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var resumeDownload = this.props.data.resumedownload;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+         </div>
+         <div className="nine columns main-col">
+            <h2>About Me</h2>
+
+            <p>Hi, I am Chidozie a Frontend Developer and Web Designer keen on creating mind-compelling and responsive websites for business. The end goal of this is to grow your traffic and hence convert.
+               TRUTH IS, YOU NEED SOLUTIONS! One thing that makes me unique; My expert ability to identify your problem and consistently offer you the solution you need. If you are looking for someone who does not only want to get paid but is willing to help take your business to the next level using the required skill, then the right person is ME.
+               I can help you create amazing and beautiful websites that will help grow your business.
+            </p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+						   <span>{name}</span><br />
+						   <span>{street}<br />
+						         {city} {state}, {zip}
+                   </span><br />
+						   <span>{phone}</span><br />
+                     <span>{email}</span>
+					   </p>
+               </div>
+               <div className="columns download">
+                  <p>
+                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;
